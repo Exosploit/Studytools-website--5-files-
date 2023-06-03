1a18f85509dd89df20a209868bb2aec4e8f6d7c1
Exosploit <128239207+Exosploit@users.noreply.github.com>
2023-06-03 12:56:09 +0200

    🌐Update 0.7
    🌟Added "Download on Github"
    🌟Added Button Animations
    🌟Added "Scroll to Download" button
    🌟Added "contact us" button
    🌟Added software licence text

--- Studytools website/eventhandler.js
+++ Studytools website/eventhandler.js
@@ -1,1 +1,6 @@
-document.getElementById("sitename").addEventListener("hover", displayDate);
+$(function() {
+    $("a[href*='#']").on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'ease-in');
+    });
+  });
